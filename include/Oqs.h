c5948a8c885a49201789e08e8f5ebc04546926e7
Dominic Meiser <dmeiser@txcorp.com>
2014-12-06 22:28:21 -0700

    Add main Oqs header.

---
+++ include/Oqs.h
@@ -0,0 +1,24 @@
+/*
+Copyright 2014 Dominic Meiser
+
+This file is part of oqs.
+
+oqs is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+oqs is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with oqs.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef OQS_H
+#define OQS_H
+
+#include <OqsAmplitude.h>
+
+#endif
